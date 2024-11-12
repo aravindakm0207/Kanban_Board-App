f602a9ac469ab5c3c756ea9de35b2a7026f3182f
Aravinda <aravindakm0207@gmail.com>
2024-11-12 16:42:40 +0530

    updated db.js to configure with render

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 const configureDB = async () => {
     try {
-        const db = await mongoose.connect(process.env.MONGODB_URI)
+        const db = await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 50000 // Increase timeout duration
+          })
         //const db = await mongoose.connect('mongodb://127.0.0.1:27017/kanban-Nov24');
         console.log("Connected to DB");
     } catch (err) {
