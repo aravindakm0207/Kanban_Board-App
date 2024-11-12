604cc7f8c9339237e1f661d213dfd529798b3237
Aravinda <aravindakm0207@gmail.com>
2024-11-12 16:23:36 +0530

    Configured render

--- frontend/my-app/src/config.js
+++ frontend/my-app/src/config.js
@@ -1,5 +1,5 @@
 // config.js
-//const API_BASE_URL = 'https://expense-app-backend-9q4b.onrender.com'; // Replace with your actual backend URL
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = `https://kanban-board-app-hnyr.onrender.com`
+//const API_BASE_URL = 'http://localhost:5000';
 export default API_BASE_URL;
 //http://localhost:5000
